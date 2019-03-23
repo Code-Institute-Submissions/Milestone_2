734e2b434fe4e4e73d9766702b5a68dff6c13ee5
Chris Anderson <traveller.anderson@gmail.com>
2019-03-23 18:41:10 +0000

    Basic Simon game logic

--- assets/js/test.js
+++ assets/js/test.js
@@ -7,30 +7,60 @@ var ctx = canvas.getContext("2d");
 function nextmoveRandom(movelist) {
   var move = parseInt((Math.random() * 4) + 1)
   if (move == 1) {
-    movelist.push('U');
+    movelist.push('ArrowUp');
   } else if (move == 2) {
-    movelist.push('D');
+    movelist.push('ArrowDown');
   } else if (move == 3) {
-    movelist.push('L');
+    movelist.push('ArrowLeft');
   } else {
-    movelist.push('R');
+    movelist.push('ArrowRight');
   }
-  return movelist
+  return movelist;
 }
+
+function checkGame(user, game){
+  userIndex = user.length - 1;
+  console.log(user[userIndex]);
+  if (user[userIndex] != game[userIndex]){
+    alert('Game Over');
+  }
+  if (user.length == game.length) {
+    userMoves = new Array();
+    nextmoveRandom(gameMoves);
+    console.log(gameMoves);
+  }
+
+}
+
+function keyDownHandler(e) {
+  console.log(e.key);
+  userMoves.push(e.key);
+  checkGame(userMoves, gameMoves);
+
+}
+
+document.addEventListener("keydown", keyDownHandler);
 
 // Create the game array 
-var gamemoves = new Array();
+var gameMoves = new Array();
 var loseState = false;
-
+var testState = false;
+var userMoves = new Array();
 // Start game with 3 moves
 for (var i = 0; i < 3; i++) {
-  gamemoves = nextmoveRandom(gamemoves);
+  gameMoves = nextmoveRandom(gameMoves);
 }
+console.log(gameMoves);
 
-while (loseState == false) {
+// while (loseState == false) {
 
-  loseState = true;
-}
+//   if (testState == true) {
+//     loseState = true;
+//     alert ("You pressed the down key!");
+
+//   }
+
+// }
 
 // ctx.beginPath();
 // ctx.rect(20, 40, 50, 50);
